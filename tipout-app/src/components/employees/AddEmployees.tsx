77d103e5dcd53d3eb4799740e71edff2796b6460
Tudor <112909151+tudor-seserman@users.noreply.github.com>
2023-08-07 11:52:04 -0500

    Some AuthContext Stuff

---
+++ tipout-app/src/components/employees/AddEmployees.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Banner from "../Banner";
+
+const AddEmployees = () => {
+  return (
+    <>
+      <Banner></Banner>
+    </>
+  );
+};
+
+export default AddEmployees;
---
+++ tipout-app/src/hooks/useLocalStorage.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+interface useLocalStorageProps {
+  keyName: string;
+  defaultValue: string;
+}
+
+export const useLocalStorage = ({
+  keyName,
+  defaultValue,
+}: useLocalStorageProps) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const value = window.localStorage.getItem(keyName);
+
+      if (value) {
+        return JSON.parse(value);
+      } else {
+        window.localStorage.setItem(keyName, JSON.stringify(defaultValue));
+        return defaultValue;
+      }
+    } catch (err) {
+      return defaultValue;
+    }
+  });
+
+  const setValue = (newValue: string) => {
+    try {
+      window.localStorage.setItem(keyName, JSON.stringify(newValue));
+    } catch (err) {}
+    setStoredValue(newValue);
+  };
+
+  return [storedValue, setValue];
+};
