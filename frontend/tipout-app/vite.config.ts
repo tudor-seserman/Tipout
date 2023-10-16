86252efe76b8d96c61d899cacb33258a9e05130e
Tudor <112909151+tudor-seserman@users.noreply.github.com>
2023-10-16 12:26:04 -0500

    Added favicon

--- frontend/tipout-app/vite.config.ts
+++ frontend/tipout-app/vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +9,7 @@ export default defineConfig({
       usePolling: true,
     },
     host: true, // needed for the Docker Container port mapping to work
-    strictPort: '0.0.0.0',
+    strictPort: true,
     port: 5173,
-}})
+  },
+});
